9352dc8d8de09beb8c832658eb3017385c35386e
App_Mark <markh@mega7.fun>
2022-02-11 15:10:13 +0800

    first commit

---
+++ mInstagram/mInstagram/Controllers/Core Tabs/HomeViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  mInstagram
+//
+//  Created by Huang YangChing on 2022/2/11.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
